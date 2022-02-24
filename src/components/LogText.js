8663e1b28c6b118978d24648cc90fc57429c1f2f
Vinícius R. Cerqueira <vinirodce@gmail.com>
2022-02-23 22:00:33 -0300

    feat: login text added

--- src/components/LogText.js
+++ src/components/LogText.js
@@ -3,11 +3,13 @@ import React from "react";
 const LogText = () => {
     const textTitle = "Olá,";
     const textDescription = "Para continuar navegando de forma segura, efetue o login na rede.";
+    const textLogin = "Login";
 
     return (
         <section>
             <h1>{textTitle}</h1> 
             <p>{textDescription}</p>
+            <h2>{textLogin}</h2>
         </section>
     );
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './Login';
 import './styles/LogButton.css';
+import './styles/LogInput.css';
 
 
 ReactDOM.render(<Login />, document.getElementById("root"));
