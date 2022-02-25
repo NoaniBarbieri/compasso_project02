6a711f12069f2049f148d74f8ee71adc79ed6077
diogojefs <diogojefs@gmail.com>
2022-02-25 03:19:07 -0300

    Feat: creating text component about company missions.

--- src/components/partials/Texts/styles.js
+++ src/components/partials/Texts/styles.js
@@ -35,3 +35,23 @@ const TextsMissionContainer = styled.div`
         display: none;
     }
 `;
+
+const TextsMission = () => {
+    return (
+        <TextsMissionContainer>
+            <TextRedMediumMission>Our mission is</TextRedMediumMission>
+            <TextBlackMission>Nossa missão é</TextBlackMission>
+
+            <TextRedGreatMission>to transform the world</TextRedGreatMission>
+            <TextBlackMission>transformar o mundo</TextBlackMission>
+
+            <TextRedGreatMission>building digital experiences</TextRedGreatMission>
+            <TextBlackMission>construindo experiências digitais</TextBlackMission>
+
+            <TextRedGreatMission>that enable our client’s growth</TextRedGreatMission>
+            <TextBlackMission>que permitam o crescimento dos nossos clientes</TextBlackMission>
+        </TextsMissionContainer>
+    )
+}
+
+export { TextsMission }
