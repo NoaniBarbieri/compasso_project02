7e29839bfc05e02d38b38d9cda498aa2f8b8db90
Noani <noanizingerbarbieri@hotmail.com>
2022-02-25 13:39:23 -0300

    Style: correction in header margin values

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../../assets/images/LogoCompasso.png"
 import { Nav } from "./Header.style";
+import { Div } from "./Header.style";
 import { Time } from "./Header.style";
 import { Days } from "./Header.style";
 import { ClimateStyle } from "./Header.style";
@@ -10,14 +11,14 @@ import GetClimate from "./GetClimate";
 
 function Header(){
     return (
-        <div>
+        <Div>
             <Nav>
                 <img src={logo} alt="projectHome"/>
                 <Time><Clock/></Time>
             </Nav>
             <Days><CalendarDate/></Days>
             <ClimateStyle><GetClimate/></ClimateStyle>
-        </div>
+        </Div>
     )
 }
 
