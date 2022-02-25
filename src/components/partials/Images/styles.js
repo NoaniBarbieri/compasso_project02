69024e1cd0323fa10c227c41cff298e4c9beb355
diogojefs <diogojefs@gmail.com>
2022-02-25 09:26:18 -0300

    Style: defining style of the ball component (logo)

---
+++ src/components/partials/Images/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Img = styled.img`
+    position: relative;
+    top: 7vh;
+    max-width: 100%;
+    height: 100%;
+    flex-shrink: 1;
+    display: block;
+`;
