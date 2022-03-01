568e0c78d61eb9a3b4c03fe5edf52508040939a8
diogojefs <diogojefs@gmail.com>
2022-02-28 22:17:53 -0300

    Style: creating styles for logout timer

--- src/components/Footer/footer.style.js
+++ src/components/Footer/footer.style.js
@@ -165,3 +165,49 @@ export const ButtonsAuth = () => {
         </ButtonFooterContainer>
     )
 }
+
+/* TimerLogout */
+const TimerLogoutContainer = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    /* padding: 0 14.67% 0 7%; */
+    padding-right: 40px;
+    border-left: 3px solid #FFF;
+    /* background-color: blue; */
+
+    span {
+        flex: 1;
+        font-size: 14px;
+        text-align: right;
+    }
+    
+    div {
+        flex: 1;
+        text-align: center;
+        margin-left: 14%;
+        /* background-color: blue; */
+
+        p {
+            font-family: 'Roboto';
+            font-size: 48px;
+        }
+    }
+
+    @media (max-width: 1023px) {
+        display: none;
+    }
+`;
+
+export const TimerToLogout = () => {
+    return (
+        <TimerLogoutContainer>
+            <span>Application <br /> refresh in</span>
+            <div>
+                <p>180</p>
+                <span>seconds</span>
+            </div>
+        </TimerLogoutContainer>
+    )
+};
