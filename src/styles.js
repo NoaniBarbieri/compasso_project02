a59657d9b1ea32e6beb7ae8bdd081c487f917599
Matheus Guimaraes <matheus.guimaraes780@gmail.com>
2022-03-03 12:47:55 -0300

    style: made login page layout responsive

--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,5 @@
 import { useInRouterContext } from 'react-router-dom';
-import styled,{createGlobalStyle} from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import MarkPro from "./assets/fonts/MarkPro.ttf";
 import MarkProBold from "./assets/fonts/MarkProBold.ttf";
 
@@ -27,29 +27,49 @@ export const GlobalStyles = createGlobalStyle`
 export const LogBackground = styled.div`
     background: linear-gradient(180deg, #33383D 0%, #1C1D20 100%);
     display: flex;
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
+
+    @media (max-height: 667px) {
+        height: auto;
+  }
 `;
 
 export const Boxlog = styled.div`
     display: flex;
     flex-direction: column;
-    height: 100%;
-    width: 50%;
+    height: 100vh;
+    width: 50vw;
     align-items: center;
+
+    @media (max-height: 667px) {
+        height: auto;
+  }
 `;
 
 export const BoxImage = styled(Boxlog)`
     background: url(${require('./assets/images/background.png')});
     background-size: cover;
     background-repeat: no-repeat;
+
+    @media (max-width: 1024px) {
+    display: none;
+  }
 `;
 
 export const Cont = styled(Boxlog)`
     display: flex;
     justify-content: center;
     max-width: 380px;
+    min-width: 380px;
     max-height: 685px;
+
+    
+
+    @media (max-width: 420px) {
+        max-width: 260px;
+        min-width: 260px;
+  }
 `;
 
 export const LogSection = styled.section`
@@ -59,7 +79,7 @@ export const LogSection = styled.section`
     justify-content: space-between;
 `;
 
-export const Title = styled.nav` //se concertado a fonte troque para styled.h1
+export const Title = styled.nav`
     height: 76px;
     margin-bottom: 15px;
 
@@ -108,7 +128,7 @@ export const InputStyle = styled.input`
    }
 
     margin: 32px 0px 32px 0px;
-    width: 100%;
+    width: 379px;
     box-sizing: border-box;
     padding: 30px 20px;
     max-height: 20px;
@@ -117,6 +137,8 @@ export const InputStyle = styled.input`
     border-style: solid;
     border-color: #E0E0E0;
     border-radius: 50px;
+
+    
 `;
 
 export const UserInputStyle = styled(InputStyle)`
@@ -143,11 +165,12 @@ export const LogIcon = styled.img`
 
 export const LogButtonStyle = styled.button`
     width: 100%;
-    max-height: 67px;
+    min-height: 67px;
     box-sizing: border-box;
     border: 2px solid;
     border-radius: 50px;
     border-color: #c13216;
+    cursor: pointer;
 
     background-image: linear-gradient(to right, #FF2D04, #c13216);
     box-shadow: inset 5px 5px 15px rgba(0, 0, 0, 0.15);
