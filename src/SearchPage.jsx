18e9c97a68f4cc4b4452548b80c3e1f7a67199fe
diogojefs <diogojefs@gmail.com>
2022-03-04 14:51:51 -0300

    Feat: creating search page and defining divisions in it.

---
+++ src/SearchPage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { LogBackground, Cont, LogButtonStyle } from "./styles"
+import { CompassLogoWhite } from "./components/partials/Images/styles"
+
+const SearchPage = () => {
+
+    return (
+        <LogBackground style={{ flexDirection: "column" }}>
+            <Cont>
+                <CompassLogoWhite />
+
+                {/* api feedback area */}
+                <div>
+                
+                </div>
+
+                {/* input area and buttons */}
+                <div>
+                    {/* input here */}
+                    
+                    <div>
+                        {/* button area */}
+                    </div>
+                </div>
+            </Cont>
+        </LogBackground>
+    )
+}
+
+export default SearchPage;
