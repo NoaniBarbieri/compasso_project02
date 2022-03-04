911ad5035dfee4a7ee758a37bfa7483c6b8f3c34
Noani <noanizingerbarbieri@hotmail.com>
2022-03-04 20:00:09 -0300

    Style: correction in the style of buttons to inherit settings already created

--- src/components/partials/Buttons/style.js
+++ src/components/partials/Buttons/style.js
@@ -51,37 +51,16 @@ export const LogButtonStyle = styled.button`
     color: #FFF;
 `;
 
-export const SearchButtonStyle = styled.button`
-    width: 379px;
-    height: 67px;
+export const SearchButtonStyle = styled(LogButtonStyle)`
     margin-bottom: 15px;
-    border: 2px solid;
-    border-radius: 50px;
-    border-color: #c13216;
-    cursor: pointer;
-    background-image: linear-gradient(to right, #FF2D04, #c13216);
-    box-shadow: inset 5px 5px 15px rgba(0, 0, 0, 0.15);
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: normal;
     font-size: 36px;
-    color: #FFF;
+    font-family: 'Roboto';
+    text-align: center;
 `;
 
-export const OutButtonStyle = styled.button`
-    width: 379px;
-    height: 67px;
-    margin-bottom: 15px;
-    border: 2px solid;
-    border-radius: 50px;
+export const OutButtonStyle = styled(SearchButtonStyle)`
     border-color: #FFFFFF;
-    cursor: pointer;
     background-image: linear-gradient(to right, #FFFFFF, #FFFFFF);
-    box-shadow: inset 5px 5px 15px rgba(0, 0, 0, 0.15);
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: normal;
-    font-size: 36px;
     color: #000000;
 `;
 
