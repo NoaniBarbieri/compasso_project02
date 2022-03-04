826822473adcc9705f982e9cb8c8e54a416a58e0
Noani <noanizingerbarbieri@hotmail.com>
2022-03-04 16:47:34 -0300

    Style: fix for button style to be reused

--- src/components/partials/Buttons/style.js
+++ src/components/partials/Buttons/style.js
@@ -35,3 +35,17 @@ export const ButtonLogout = styled(ButtonSquareFooter)`
     background-color: transparent;
     color: #FFF;
 `;
+
+export const LogButtonStyle = styled.button`
+    width: clamp(100px,80vw,379px);
+    padding: clamp(7px,3vh,20px) 20px;
+    border: 2px solid;
+    border-radius: 50px;
+    border-color: #c13216;
+    cursor: pointer;
+    background-image: linear-gradient(to right, #FF2D04, #c13216);
+    box-shadow: inset 5px 5px 15px rgba(0, 0, 0, 0.15);
+    font-family: MarkProBold, sans-serif;
+    font-size: 18px;
+    color: #FFF;
+`;
--- src/styles.js
+++ src/styles.js
@@ -137,19 +137,6 @@ export const LogIcon = styled.img`
    padding: 17px 20px 17px 336px;
 `;
 
-export const LogButtonStyle = styled.button`
-    width: clamp(100px,80vw,379px);
-    padding: clamp(7px,3vh,20px) 20px;
-    border: 2px solid;
-    border-radius: 50px;
-    border-color: #c13216;
-    cursor: pointer;
-    background-image: linear-gradient(to right, #FF2D04, #c13216);
-    box-shadow: inset 5px 5px 15px rgba(0, 0, 0, 0.15);
-    font-family: MarkProBold, sans-serif;
-    font-size: 18px;
-    color: #FFF;
-`;
 
 export const LogInvalid = styled.nav`
     display: flex;
