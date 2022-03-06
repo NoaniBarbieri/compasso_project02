605ac4a4cd30effe5beb0edff67af796528e5fb6
Noani <noanizingerbarbieri@hotmail.com>
2022-03-06 19:52:51 -0300

    Style: fix  box-shadow in buttons

--- src/components/partials/Buttons/style.js
+++ src/components/partials/Buttons/style.js
@@ -44,8 +44,8 @@ export const LogButtonStyle = styled.button`
     border-radius: 50px;
     border-color: #c13216;
     cursor: pointer;
-    background-image: linear-gradient(to right, #FF2D04, #c13216);
-    box-shadow: inset 5px 5px 15px rgba(0, 0, 0, 0.15);
+    background-image: linear-gradient(to right, #FF2D04, #C13216);
+    box-shadow: inset 5px 5px 15px rgba(0, 0, 0, 0.15),5px 5px 15px #0b0b0b;
     font-family: MarkProBold, sans-serif;
     font-size: 18px;
     color: #FFF;
