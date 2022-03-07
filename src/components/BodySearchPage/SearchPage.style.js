1cb968d7bb0a4ae8dcb29ac8e32d95fc5f5664d9
Noani <noanizingerbarbieri@hotmail.com>
2022-03-07 12:52:05 -0300

    Style: mobile mode

--- src/components/BodySearchPage/SearchPage.style.js
+++ src/components/BodySearchPage/SearchPage.style.js
@@ -7,6 +7,7 @@ export const SearchContainer = styled.div`
     min-height: 60px;
     max-width: 516px ;
     width: 100%;
+    flex: 2;
 
     @media (max-width: 1023px) {
         input {
@@ -14,7 +15,7 @@ export const SearchContainer = styled.div`
             height: 50px;
             background-size: 12px;
             font-size: 20px;
-            margin-bottom: 10px;
+            margin-bottom: 8px;
         }
 
         button {
@@ -25,8 +26,42 @@ export const SearchContainer = styled.div`
             display:flex;
             justify-content: center;
             align-items: center ;
+            margin-bottom: 5px;
         }
 
     }
 
 `  
+
+export const APIContainer = styled.div`
+    display: flex ;
+    align-items: center ;
+    justify-content: center ;
+    flex-direction: column ;
+    margin-top: 20px ;
+    max-height: 560px;
+    max-width:  611px ;
+    height: 200px;
+
+    p{
+        width: 100%;
+        height: 65%;
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 40px;
+    }
+
+    @media (max-width: 1023px) {
+
+        p{
+            width: 80%;
+            height:  280px;
+            font-size: 20px;
+            margin-bottom: 10px;
+        }
+    }
+
+    
+    
+`
