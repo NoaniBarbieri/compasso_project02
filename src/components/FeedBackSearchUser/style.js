59d80f3dc7579087ffc92e930cd41751dfa0f459
diogojefs <diogojefs@gmail.com>
2022-03-07 12:07:48 -0300

    Style: new component for user feedback and search.

---
+++ src/components/FeedBackSearchUser/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ContainerFeedbackUserSearch = styled.div`
+    max-width: 516px;
+    width: 100%;
+    white-space: pre-line;
+    /* background-color: blue; */
+    display: flex;
+    justify-content: center;
+
+    p {
+        font-family: 'Roboto';
+        font-size: clamp(20px,3.5vw,25px);
+        text-align: left;
+        color: #fff; 
+    }
+`
